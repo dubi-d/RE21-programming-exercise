c70a3b89bcdec969ef0064fd1979046e859799db
wenkf <wenkf@student.ethz.ch>
2021-05-28 15:51:35 +0200

    Added measurement probability

--- Code/P2_ParticleFilter_template/Estimator.m
+++ Code/P2_ParticleFilter_template/Estimator.m
@@ -78,6 +78,12 @@ end % end init
 [x_r, y_r, phi, kappa] = move_particles(prevPostParticles, act, estConst, N_particles);
 
 % Posterior Update:
+[basePts, vecs] = initContourVectors(estConst.contour);
+t_min = calcMinDistances(basePts, vecs, x_r, y_r, phi, kappa);
+p_meas = measurementProbabilities(t_min, sens, estConst.epsilon);
+
+weights = p_meas/sum(p_meas);
+
 
 % % postParticles.x_r = ...
 % % postParticles.y_r = ...
@@ -93,71 +99,114 @@ uni = (rand(1, N) - 0.5) * 2 * bound;
 end
 
 function [x_samples, y_samples] = sample_init_positions(N, estConst)
-pA = estConst.pA; % center of circle A
-pB = estConst.pB; % center of circle B
-r = estConst.d; % radius of circles
+    pA = estConst.pA; % center of circle A
+    pB = estConst.pB; % center of circle B
+    r = estConst.d; % radius of circles
 
-x_samples = zeros(1, N);
-y_samples = zeros(1, N);
+    x_samples = zeros(1, N);
+    y_samples = zeros(1, N);
 
-for i = 1:N
-    isA = (rand < 0.5); % choose circle
-    xi = (rand - 0.5) * 2 * r; % sample x in generic circle
-    yBound = sqrt(r^2 - xi^2); % compute range of posible y given x
-    yi = (rand - 0.5) * 2 * yBound; % sample y
-    x_samples(i) = xi + pA(1) * isA + pB(1) * (~isA); % shift to given circle
-    y_samples(i) = yi + pA(2) * isA + pB(2) * (~isA);
-end
+    for i = 1:N
+        isA = (rand < 0.5); % choose circle
+        xi = (rand - 0.5) * 2 * r; % sample x in generic circle
+        yBound = sqrt(r^2 - xi^2); % compute range of posible y given x
+        yi = (rand - 0.5) * 2 * yBound; % sample y
+        x_samples(i) = xi + pA(1) * isA + pB(1) * (~isA); % shift to given circle
+        y_samples(i) = yi + pA(2) * isA + pB(2) * (~isA);
+    end
 end
 
 function [x_r, y_r, phi, kappa] = move_particles(part, act, estConst, N)
-% process noise is ~Uniform(-bound, bound)
-f_bound = estConst.sigma_f;
-phi_bound = estConst.sigma_phi;
+    % process noise is ~Uniform(-bound, bound)
+    f_bound = estConst.sigma_f;
+    phi_bound = estConst.sigma_phi;
 
-% vectorize inputs
-u_f = repmat(act(1), 1, N);
-u_phi = repmat(act(2), 1, N);
+    % vectorize inputs
+    u_f = repmat(act(1), 1, N);
+    u_phi = repmat(act(2), 1, N);
 
-% apply process model including noise
-x_r = part.x_r + (u_f + get_uni_vec(f_bound, N)) .* cos(part.phi);
-y_r = part.y_r + (u_f + get_uni_vec(f_bound, N)) .* sin(part.phi);
-phi = part.phi + u_phi + get_uni_vec(phi_bound, N);
-kappa = part.kappa;
+    % apply process model including noise
+    x_r = part.x_r + (u_f + get_uni_vec(f_bound, N)) .* cos(part.phi);
+    y_r = part.y_r + (u_f + get_uni_vec(f_bound, N)) .* sin(part.phi);
+    phi = part.phi + u_phi + get_uni_vec(phi_bound, N);
+    kappa = part.kappa;
 end
 
 function [particlesResampled, weightsResampled] = resample(particles, weights)
 
-% systematic resampling (low variance)
+    % systematic resampling (low variance)
 
-% weights are already normalized beforehand (normalization of weights is required or this won't work)
+    % weights are already normalized beforehand (normalization of weights is required or this won't work)
 
-numSpokes = size(particles, 1);  % number of spokes of the resampling wheel (= number of particles to sample)
-u = rand() / numSpokes;  % first spoke's position along the arc of the wheel
-sumWeights = weights(1);  % initialize accumulating sum of weights (= arc length covered so far)
-j = 1;
-particlesResampled = zeros(size(particles));
-weightsResampled = zeros(size(weights));
+    numSpokes = size(particles, 1);  % number of spokes of the resampling wheel (= number of particles to sample)
+    u = rand() / numSpokes;  % first spoke's position along the arc of the wheel
+    sumWeights = weights(1);  % initialize accumulating sum of weights (= arc length covered so far)
+    j = 1;
+    particlesResampled = zeros(size(particles));
+    weightsResampled = zeros(size(weights));
 
-% going through all the spokes
-for i = 1:numSpokes
-    
-   % check which particle (= arc segment with length of its weight) gets hit by the current spoke
-   while sumWeights < u
-       j = j + 1;
-       sumWeights = sumWeights + weights(j);
-       
-   end
-   
-   % select the sample that gets hit by the current spoke
-   particlesResampled(i, :) = particles(j, :);
-   weightsResampled(i) = weights(j);
-   
-   u = u + 1/numSpokes;  % move on to the next spoke
-    
+    % going through all the spokes
+    for i = 1:numSpokes
+
+       % check which particle (= arc segment with length of its weight) gets hit by the current spoke
+       while sumWeights < u
+           j = j + 1;
+           sumWeights = sumWeights + weights(j);
+
+       end
+
+       % select the sample that gets hit by the current spoke
+       particlesResampled(i, :) = particles(j, :);
+       weightsResampled(i) = weights(j);
+
+       u = u + 1/numSpokes;  % move on to the next spoke
+
+    end
+
+    % re-normalize the new weights
+    weightsResampled = weightsResampled / sum(weightsResampled);
+
 end
 
-% re-normalize the new weights
-weightsResampled = weightsResampled / sum(weightsResampled);
+%% Distance calculation
+function [basePts, vecs] = initContourVectors(contour)
+    contour = [contour; contour(1,:)];
+    vecs = diff(contour);
+    basePts = contour(1:end-1, :);
+end
+
+function [basePts, vecs] = updateContour(basePts, vecs, kappa)
+    basePts(8:9, 1) = kappa;
+    vecs(7, 1) = vecs(7,1) + kappa;
+    vecs(9, 1) = vecs(9,1) - kappa;
+end
+
+function [tmin] = calcMinDistParticle(basePts, vecs, px, py, phi, kappa)
+    [basePts, vecs] = updateContour(basePts, vecs, kappa);
     
+    p = [px, py];
+    r = [cos(phi), sin(phi)];
+    
+    t = diff((p - basePts) .* fliplr(vecs), 1, 2) ./ diff(fliplr(r) .* vecs, 1, 2);
+    s = diff((basePts - p) .* fliplr(r), 1, 2) ./ diff(fliplr(vecs) .* r, 1, 2);
+    
+    tmin = min(t((s >= 0) & (s <= 1) & (t >= 0)));
+end
+
+function t_mins = calcMinDistances(basePts, vecs, px, py, phi, kappa)
+    n_pts = length(phi);
+    t_mins = zeros(n_pts, 1);
+    for i = 1:n_pts
+        t_mins(i) = calcMinDistParticle(basePts, vecs, px(i), py(i), phi(i), kappa(i));
+    end
+end
+
+%% Measurement Probability
+
+function measProb = measurementProbabilities(t, z, epsilon)
+    d = abs(t-z);
+    
+    measProb = zeros(size(d));
+    measProb(d < 2*epsilon) = 1/(5*epsilon)*(2 - d(d < 2*epsilon)/epsilon);
+    measProb(d > 2*epsilon & d < 3*epsilon) = 1/(5*epsilon)*(1- 2*abs(d(d > 2*epsilon & d < 3*epsilon)-2.5*epsilon)/epsilon);
 end
