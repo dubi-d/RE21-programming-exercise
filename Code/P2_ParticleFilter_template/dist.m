43108b22a59a51228e4ee26da8fdac9e745b70b9
wenkf <wenkf@student.ethz.ch>
2021-05-28 17:45:38 +0200

    Checkt distance calculation with plot

--- Code/P2_ParticleFilter_template/dist.m
+++ Code/P2_ParticleFilter_template/dist.m
@@ -1,10 +1,45 @@
-const = EstimatorConst();
-[basePts, vecs] = initContourVectors(const.contour);
-n_parts = 20000;
-tic
+simConst = EstimatorConst();
+[basePts, vecs] = initContourVectors(simConst.contour);
+% n_parts = 20000;
+% tic
 % [tmin] = calcMinDistances(basePts, vecs, ones(n_parts,1)*[2], ones(n_parts,1)*[0.5], ones(n_parts,1)*[5*pi/4], -0.2:0.4/(n_parts):0.2);
-tmins = calcMinDistances(basePts, vecs, [1], [1], [0], [0])
-toc
+% toc
+
+n_particles = 20;
+px = [rand(1, n_particles)*3, 2, 1.5];
+py = [rand(1, n_particles)*3, 2, 1.5];
+phi = [rand(1, n_particles)*2*pi, pi, pi];
+kappa = [zeros(1,n_particles), 0.1, -0.1];
+tmin = calcMinDistances(basePts, vecs, px, py, phi, kappa);
+
+% Plot
+figure(1)
+clf
+% Plot contour
+spacing = 0.6;
+contour = simConst.contour;
+contour(8, 1) = contour(8, 1);
+contour(9, 1) = contour(9, 1);
+axis([min(contour(:,1))-spacing,max(contour(:,1))+spacing,...
+      min(contour(:,2))-spacing,max(contour(:,2))+spacing])
+hold on
+for i = 1:size(contour,1)-1
+    plot([contour(i,1),contour(i+1,1)],[contour(i,2),contour(i+1,2)],'k--')
+end
+plot([contour(end,1),contour(1,1)],[contour(end,2),contour(1,2)],'k--')
+
+% Plot particles
+scatter(px, py, 'bo')
+for i = 1:length(px)
+    line([px(i); px(i)+10*cos(phi(i))], [py(i); py(i)+10*sin(phi(i))])
+    scatter(px(i)+tmin(i).*cos(phi(i)), py(i)+tmin(i).*sin(phi(i)), 'rx')
+end
+
+hold off
+axis([min(contour(:,1))-spacing,max(contour(:,1))+spacing,...
+      min(contour(:,2))-spacing,max(contour(:,2))+spacing])
+
+%%
 
 function [basePts, vecs] = initContourVectors(contour)
     contour = [contour; contour(1,:)];
